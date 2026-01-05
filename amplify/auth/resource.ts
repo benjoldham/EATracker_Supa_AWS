2936b6317fdfd7a86e9f3c9637b3c1cf470f090b
benjoldham <benjoldham17@gmail.com>
2026-01-05 10:51:10 +0000

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,14 +4,4 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-
-  // Enable hosted/redirect login (required for signInWithRedirect)
-  redirectUrls: {
-    callbackUrls: [
-      "https://main.d1qq9xnqrsdy22.amplifyapp.com/aws-test.html",
-    ],
-    logoutUrls: [
-      "https://main.d1qq9xnqrsdy22.amplifyapp.com/aws-test.html",
-    ],
-  },
 });
