46b2292d124401da754ee337f1eff780f022bb09
benjoldham <benjoldham17@gmail.com>
2026-01-15 23:00:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -681,7 +681,7 @@ function render(){
       <td><div class="cell-contain">${avgDisplay}</div></td>
       <td><div class="cell-contain wide"><span class="badge ${badgeClass(status)}">${status}</span></div></td>
       <td><div class="cell-contain wide">${escapeHtml(p.seniority || "Senior")}</div></td>
-      <td><div class="cell-contain">${p.homegrown ? "🌱" : ""}</div></td>
+      <td><div class="cell-contain">${p.homegrown ? "Y" : "N"}</div></td>
       <td class="num"><div class="cell-contain"><span class="val-neg">${fmtMoneyAbbrevFromGBP(p.cost_gbp || 0, currency)}</span></div></td>
       <td class="num"><div class="cell-contain">${saleCell}</div></td>
       <td class="num"><div class="cell-contain"><span class="${valClassFromNumber(profGBP)}">${fmtMoneyAbbrevFromGBP(profGBP, currency)}</span></div></td>
